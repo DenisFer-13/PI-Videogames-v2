674bde94b1e776f27a5cf892f88e566ac8d7ff54
Denis <82286878+DenisOrlando@users.noreply.github.com>
2021-09-10 15:41:40 -0300

    inicializado el proyecto

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(1400),
+      allowNull: true,
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.NUMBER,
+      min: 1,
+      max: 5,
+      allowNull: true,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    background_image: {
+      type: DataTypes.STRING(600),
+      allowNull: true,
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  });
+};
